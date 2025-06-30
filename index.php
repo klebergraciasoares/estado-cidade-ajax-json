7053c3b7b00c2298330c75ed796d717597ac4c14
DEV05\leona <leonardo.coelho@beresolution.com.br>
2025-06-30 09:47:36 -0300

    Adicionado autor... By KGS

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>Busca Ajax de Estado e Cidade com PHP, jQuery e JSON</title>
+    <title>Busca Ajax de Estado e Cidade com PHP, jQuery e JSON - By Kleber Gracia Soares</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
